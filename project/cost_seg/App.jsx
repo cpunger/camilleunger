d837b47562db831d5f48252304a2864325c6cda4
Camille Unger <camillepaulaunger@gmail.com>
2026-04-16 15:02:14 -0500

    Create App.jsx that imports and renders CostSegAnalyzer component

---
+++ project/cost_seg/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CostSegAnalyzer from './CostSegAnalyzer';
+
+const App = () => {
+    return (
+        <div>
+            <CostSegAnalyzer />
+        </div>
+    );
+};
+
+export default App;
